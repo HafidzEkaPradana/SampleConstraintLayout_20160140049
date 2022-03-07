2253aef45c8c5df6dc641441bc1556d126c491a5
HafidzEkaPradana <hafidz.eka.2016@ft.umy.ac.id>
2022-03-07 19:37:49 +0700

    Done Activity 3

--- app/src/main/java/com/example/sampleconstraintlayout/ActivityHasil.java
+++ app/src/main/java/com/example/sampleconstraintlayout/ActivityHasil.java
@@ -1,4 +1,42 @@
 package com.example.sampleconstraintlayout;
 
-public class ActivityHasil {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityHasil extends AppCompatActivity {
+
+    TextView txEmail, txPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+
+        //menghubungkan variable txEmail dengan component TextView pada layout
+        txEmail = findViewById(R.id.tvEmail2);
+
+        //menghubungkan variable txPassword dengan component TextView pada layout
+        txPassword = findViewById(R.id.tvPassword2);
+
+        //Mendeklarasikan bundle yang akan digunakan untuk mengambil pesan
+        // atau nilai yang dikirimkan melalui intent dari activity pertama ke activity kedua.
+
+        Bundle bundle = getIntent().getExtras();
+
+        //Buat sintak yang berfungsi untuk mengambil nilai yang dikirimkan dengan menggunakan
+        // intent dari activity pertama ke activity kedua. Method yang digunakan
+        // adalah method “bundle.getString(“key”)” dan kemudian simpan nilai
+        // tersebut kedalam variabel dengan tipe data string.
+
+        String email = bundle.getString("a");
+        String pass = bundle.getString("b");
+
+        //Tampilkan nilai tersebut kedalam textview dengan menggunakan method
+        // “TextView.setText(value)”.
+
+        txEmail.setText(email);
+        txPassword.setText(pass);
+    }
 }
--- app/src/main/java/com/example/sampleconstraintlayout/ActivityKedua.java
+++ app/src/main/java/com/example/sampleconstraintlayout/ActivityKedua.java
@@ -1,4 +1,46 @@
 package com.example.sampleconstraintlayout;
 
-public class ActivityKedua {
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import  androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityKedua extends AppCompatActivity{
+
+    //mendeklarasikan variable dengan tipe data textview
+    TextView txEmail, txPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kedua);
+
+        //menghubungkan variable txEmail dengan component TextView pada layout
+        txEmail = findViewById(R.id.tvEmail);
+
+        //menghubungkan variable txPassword dengan component TextView pada layout
+        txPassword = findViewById(R.id.tvPassword);
+
+        //Mendeklarasikan bundle yang akan digunakan untuk mengambil pesan
+        // atau nilai yang dikirimkan melalui intent dari activity pertama ke activity kedua.
+
+        Bundle bundle = getIntent().getExtras();
+
+        //Buat sintak yang berfungsi untuk mengambil nilai yang dikirimkan dengan menggunakan
+        // intent dari activity pertama ke activity kedua. Method yang digunakan
+        // adalah method “bundle.getString(“key”)” dan kemudian simpan nilai
+        // tersebut kedalam variabel dengan tipe data string.
+
+        String email = bundle.getString("a");
+        String pass = bundle.getString("b");
+
+        //Tampilkan nilai tersebut kedalam textview dengan menggunakan method
+        // “TextView.setText(value)”.
+
+        txEmail.setText(email);
+        txPassword.setText(pass);
+
+
+    }
 }
