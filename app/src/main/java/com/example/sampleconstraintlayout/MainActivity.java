aca88c03112b3114ff414673f121594a43024c38
HafidzEkaPradana <hafidz.eka.2016@ft.umy.ac.id>
2022-03-07 18:54:36 +0700

    Design activity_kedua.xml done

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sampleconstraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
     //Deklarasi variable untuk menyimpan email dan password
     String nama, password;
-
-    String email, passwordAnda;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +36,11 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                email = "hafidzekapradana@gmail.com";
-                passwordAnda = "hafidz2022";
+
+                //set isi email yang benar
+                String email = "hafidzekapradana@gmail.com";
+                //set password yang benar
+                String passwordAnda = "hafidz2022";
 
 
                 //menyimpan input user diedittext email kedalam variable name
@@ -50,20 +53,38 @@ public class MainActivity extends AppCompatActivity {
                 //Toast t = Toast.makeText(getApplicationContext(), "Email Anda : "+nama+ "dan Password anda : " +password+"",Toast.LENGTH_LONG);
                 //menampilkan toast
                 //t.show();
-
-                if((nama.equals(email)) && (password.equals(passwordAnda))){
-                    Toast t = Toast.makeText(getApplicationContext(), "Login Sukses",Toast.LENGTH_LONG);
-                    t.show();
-                }else if((nama.equals(email)) && (password != passwordAnda)){
-                    Toast t = Toast.makeText(getApplicationContext(), "Password Salah",Toast.LENGTH_LONG);
-                    t.show();
-                }else if((nama != email) && (password.equals(passwordAnda))){
-                    Toast t = Toast.makeText(getApplicationContext(), "Email Salah",Toast.LENGTH_LONG);
+                if(nama.isEmpty() || password.isEmpty()){
+                    Toast t = Toast.makeText(getApplicationContext(), "Email dan Password wajib diisi", Toast.LENGTH_SHORT);
                     t.show();
                 }else{
-                    Toast t = Toast.makeText(getApplicationContext(), "Email dan Password Salah",Toast.LENGTH_LONG);
-                    t.show();
+                    //mengecek apakah email dan password sudah sama dengan email dan password
+                    //yang sudah diset
+                    if(nama.equals(email) && password.equals(passwordAnda)){
+                        Toast t = Toast.makeText(getApplicationContext(), "Login Sukses",Toast.LENGTH_LONG);
+                        t.show();
+
+                        Bundle b = new Bundle();
+
+                        //memasukkan value dari variable nama dengan kunci "a"
+                        //dan dimasukkan dibundle
+
+                        b.putString("a", nama.trim());
+                        //memasukkan value dari variable nama dengan kunci "b"
+                        //dan dimasukkan dibundle
+
+                        b.putString("b", password.trim());
+
+                        //membuat object indent berpindah activity dari mainactivity ke activityhasil
+                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+
+                        //memasukkan bundle ke dalam intent untuk dikirimkan ke activityHasil
+                        i.putExtras(b);
+                        //berpindah ke ActivityHasil
+                        startActivity(i);
+                    }
                 }
+
+
             }
         });
     }
